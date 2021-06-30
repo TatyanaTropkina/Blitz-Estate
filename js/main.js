8a6ebb116935ab3f78b9b5251812a649ed3cb8a5
TatyanaTropkina <tropkina.tatyana@gmail.com>
2021-07-01 00:32:50 +0300

    update index

--- js/main.js
+++ js/main.js
@@ -2,25 +2,37 @@ $(function () {
     $('.apart__gallery').slick({
         fade: true,
         arrows: false,
-        swipe: true,
-
     });
+	$('.quiz__slider').slick({
+		prevArrow: false,
+		nextArrow: '<button class="quiz__slider-btn">Продолжить</button>',
+    });
+	
+	var mixer = mixitup('.apart__items', {
+		load: {
+			filter: '.all'
+		},
+		animation: {
+			effectsIn: 'fade translateY(0) translateX(0)',
+			effectsOut: 'fade translateY(0) translateX(0)',
+			effects: 'fade translateY(0) translateY(0)',
+			duration: 500
+		}
+		
+	});
     $('.apart__gallery').on('click', function () {
         $(this).slick('slickNext');
     });
+$('.apart__items-more').hide();
+$('.apart__inner-btn').on('click', function(){
+	$('.apart__items-more').show();
+	$(this).hide();
+});
+$('.apart__btn').on('click', function(){
+	$('.apart__items-more').hide();
+	$('.apart__inner-btn').show();
 
-
-    $('.apart__content-link-more').on('click', function () {
-        $(this).css('display', 'none');
-        $('.apart__items.hide').toggleClass('show-more');
-        $('.apart__content-link-less').css('display', 'block');
-    })
-    $('.apart__content-link-less').on('click', function () {
-        $(this).css('display', 'none');
-        $('.apart__items.hide').removeClass('show-more');
-        $('.apart__content-link-more').css('display', 'block');
-
-    })
+})
     $('.about__gallery-slider').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -33,7 +45,6 @@ $(function () {
         slidesToScroll: 1,
         arrows: false,
         focusOnSelect: true,
-        centerPadding: '0px',
         asNavFor: '.about__gallery-slider',
 
     });
@@ -74,48 +85,48 @@ $(function () {
 });
 
 
-filterSelection("all")
+// filterSelection("all")
 
-function filterSelection(c) {
-    var x, i;
-    x = document.getElementsByClassName("apart__item");
-    if (c == "all") c = "";
+// function filterSelection(c) {
+//     var x, i;
+//     x = document.getElementsByClassName("apart__item");
+//     if (c == "all") c = "";
 
-    for (i = 0; i < x.length; i++) {
-        w3RemoveClass(x[i], "show");
-        if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-    }
-}
+//     for (i = 0; i < x.length; i++) {
+//         w3RemoveClass(x[i], "show");
+//         if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+//     }
+// }
 
-function w3AddClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-        if (arr1.indexOf(arr2[i]) == -1) {
-            element.className += " " + arr2[i];
-        }
-    }
-}
+// function w3AddClass(element, name) {
+//     var i, arr1, arr2;
+//     arr1 = element.className.split(" ");
+//     arr2 = name.split(" ");
+//     for (i = 0; i < arr2.length; i++) {
+//         if (arr1.indexOf(arr2[i]) == -1) {
+//             element.className += " " + arr2[i];
+//         }
+//     }
+// }
 
-function w3RemoveClass(element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-        while (arr1.indexOf(arr2[i]) > -1) {
-            arr1.splice(arr1.indexOf(arr2[i]), 1);
-        }
-    }
-    element.className = arr1.join(" ");
-}
+// function w3RemoveClass(element, name) {
+//     var i, arr1, arr2;
+//     arr1 = element.className.split(" ");
+//     arr2 = name.split(" ");
+//     for (i = 0; i < arr2.length; i++) {
+//         while (arr1.indexOf(arr2[i]) > -1) {
+//             arr1.splice(arr1.indexOf(arr2[i]), 1);
+//         }
+//     }
+//     element.className = arr1.join(" ");
+// }
 
-var btnContainer = document.getElementById("apart__filter");
-var btns = btnContainer.getElementsByClassName("apart__filter-btn");
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function () {
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
-    });
-}
+// var btnContainer = document.getElementById("apart__filter");
+// var btns = btnContainer.getElementsByClassName("apart__filter-btn");
+// for (var i = 0; i < btns.length; i++) {
+//     btns[i].addEventListener("click", function () {
+//         var current = document.getElementsByClassName("active");
+//         current[0].className = current[0].className.replace(" active", "");
+//         this.className += " active";
+//     });
+// }
